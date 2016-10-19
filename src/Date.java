a45493b8fb4be5fa0afc531c51a4cf05eead9955
Tehnikumi Õpilane <opilane@tptlive.ee>
2016-10-19 16:21:58 +0300

    Initial commit

---
+++ src/Date.java
@@ -0,0 +1,38 @@
+
+public class Date {
+    public static void main(String[]args){
+
+        System.out.println("Hello World!");
+        System.out.println();
+        // creating hour and minute
+        //int hour = 11;
+        //int minute = 59;
+
+        // creating day, month, year
+        String day = "Wednesday";
+        int date = 19;
+        String month = "October";
+        int year = 2016;
+
+
+        /*
+
+        //testing..
+        System.out.println(day);
+        System.out.println(date);
+        System.out.println(month);
+        System.out.println(year);
+        //success!
+        */
+
+        //excercise 2.2
+
+        System.out.println("American format:");
+        System.out.println(day + ", " + month + " " + date + ", " + year);
+        System.out.println("European format:");
+        System.out.println(day + " " + date + " " + month + " " + year);
+
+
+
+    }
+}
